e941285d676f11156e9bae2a219745706c056764
Hung Lin <hung.lin@audaxhealth.com>
2014-10-30 04:40:38 -0400

    v0.2 - use RDD to sort rank

--- common-crawl-data/build.sbt
+++ common-crawl-data/build.sbt
@@ -1,7 +1,7 @@
 name := "Common Crawl Data"
 
-version := "0.1"
+version := "0.2"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.10.3"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.1.0"
--- common-crawl-data/src/main/scala/CommonCrawlData.scala
+++ common-crawl-data/src/main/scala/CommonCrawlData.scala
@@ -1,17 +1,27 @@
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import scala.util.{Failure, Success, Try}
 
 object CommonCrawlData {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Common Crawl Data")
     val sc = new SparkContext(conf)
+
     //TODO: Use Apache Commons CLI
     val data = sc.textFile(args(0))
+
     val uriLines = data.filter(_.startsWith("WARC-Target-URI: "))
-    val hosts = uriLines.map(line => { scala.util.Try { line.split("http://").apply(1).split("/").apply(0) }.toOption.getOrElse("PARSE_ERROR") } )
-    val hostPageNumber = hosts.countByValue
-    val rank = hostPageNumber.toSeq.sortBy(_._2 * -1)
-    rank.foreach(x => {println(s"${x._1},${x._2}")})
+    val hosts = uriLines.map(line => {
+      Try { line.split("http://").apply(1).split("/").apply(0) } match {
+        case Success(x) => x
+        case Failure(exception) => "PARSE_ERROR_" + exception.getMessage
+      }
+    })
+    val hostPageNum = hosts.countByValue
+    val rank = sc.parallelize(hostPageNum.toSeq).sortBy(_._2 * -1)
+
+    rank.foreach(x => {
+      println(s"${x._1},${x._2}")
+    })
   }
 }
